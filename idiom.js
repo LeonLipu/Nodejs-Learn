43b1f84cc26b9d979fe1f5b848f0336077ac16a0
brahmanda kar <you@example.com>
2015-09-19 03:00:54 -0700

    contains idiom

---
+++ idiom.js
@@ -0,0 +1,5 @@
+//console contains
+
+var str="i have something to say and it would be better and which need to have so that ";
+ if(~str.indexOf("something"));
+ console.log("string got match with idioms ");
