4ea5323d29818b6bb57ede4ea7888563566c6b77
git <lipusipupara@gmail.com>
2015-10-19 11:36:28 +0530

    get date functions

--- datefunction.js
+++ datefunction.js
@@ -1,2 +1,23 @@
+
+//date produce like Mon Oct 19 2015 11:09:28 GMT+0530 (India Standard Time)
 var d=Date();
 console.log(d);
+
+//date as object
+//Date [year ,month,hour,minutes,seconds,milliseconds]
+var today=new Date(1995,11,2);
+
+console.log(today);//Sat Dec 02 1995 00:00:00 GMT+0530
+
+//for unix timestamp
+var unixtimestamp=Date.now();//1445234106028  this epoc time
+console.log(unixtimestamp);
+
+
+
+var doj=new Date();
+console.log(doj.getDate());
+console.log(doj.getDay());
+console.log(doj.getFullYear());
+console.log(doj.getMonth());
+console.log(doj.getSeconds());
