47bc61114ed79af4251926615d0c033b94deb2e1
git <lipusipupara@gmail.com>
2015-11-23 22:41:26 +0530

    global object

--- process.js
+++ process.js
@@ -1,7 +1,15 @@
+
+
+var fs  = require('fs ');
+var child_process = require('child_process');
+
+//Global object
+console.log(__filename);
+console.log(__dirname);
+
 process.on('exit', function() {
   console.log("i am exiting ");
 });
-
 
 process.on('uncaughtException',function(code){
 
