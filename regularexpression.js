0176a94332878be6f4f354beed9e340d962c2a4f
LeonLipu <lipusipupara@gmail.com>
2015-11-30 02:35:34 +0530

    Create regularexpression.js

---
+++ regularexpression.js
@@ -0,0 +1,11 @@
+var re = new RegExp('[a-z]*');
+var result = re.exec("something123");
+console.log(result);
+
+var re2 = new RegExp("[a-z]","gi"); //it will not work
+console.log(re2.global);
+var result2= re2.exec('something123 nothing');
+console.log(result2);
+
+var result3="something and something ".match(/something/g);//best way to match
+console.log(result3);
