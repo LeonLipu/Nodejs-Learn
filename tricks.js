e523c86a9afdb1d9c2d3864da28f4ef00705ff8a
leonlipu <lipusipupara@gmail.com>
2015-12-17 23:46:31 +0530

    trick about global scope

---
+++ tricks.js
@@ -0,0 +1,9 @@
+//speical tricks
+
++function (){
+  var a=b=6;
+}();
+
+
+console.log(typeof(a)); //why undefied  becoz var a=b=5 is equal to b=0 var a=b and b become global variable to display outside
+console.log(typeof(b));
