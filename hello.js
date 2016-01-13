a5aa67835271fb8dded35e80dfcfde1907e2a273
git <lipusipupara@gmail.com>
2016-01-13 23:43:07 +0530

    type of NaN  is number

--- hello.js
+++ hello.js
@@ -6,3 +6,10 @@
 
 console.log(typeof(a)); //why undefied  becoz var a=b=5 is equal to b=0 var a=b and b become global variable to display outside
 console.log(typeof(b));
+
+
+console.log(typeof(NaN));//number
+
+if(typeof(NaN)=== 'number' ){
+  console.log("yes it is true ");
+}
