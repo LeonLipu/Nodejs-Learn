17cbafb59a1567ec428469b5b23ab8328cc63cef
git <lipusipupara@gmail.com>
2016-01-14 23:56:09 +0530

    built-in standard objcets

---
+++ doument/aboutobject.js
@@ -0,0 +1,84 @@
+  some standard object in java script
+
+  1.value properties (global property )
+  infinity
+  NaN
+  undefined
+  null
+
+  2.function properties(global function )
+eval()
+isfinite()
+parseFloat()
+parseInt()
+decodeURI()
+decodeURIcomponent()
+encodeURI()
+encodeURIcomponent ()
+
+  3.fundamental object
+
+  Object
+  function
+  Boolean
+  Symbol
+  Error
+  EvalError
+  InternalError
+  RangeError
+  Reference Error
+  SyntaxError
+  TypeError
+  URIError
+
+  4.number and date
+  number
+  Math
+  date
+
+  5.indexed collection
+  Array
+  int8array
+  Uint8array
+  UintClampedArray
+  Int16Array
+  Uint16Array
+  int32Array
+  Uint32Array
+  Float32Array
+  Float64Array
+
+
+
+  6.keyed collection
+  Map
+  Set
+  WeakMap
+  WeakSet
+
+  7.structerd data
+ArrayBuffer
+DataView
+Json
+
+
+  8.control abstraction
+promise
+generator
+generator function
+
+  9.reflection
+reflect
+proxy
+
+  10.internatilationn
+intl
+intl.collator
+intl.DateTimeFormat
+intl.NumberFormat
+arguments
+
+
+  11.Text processing
+  String
+  Regex 
