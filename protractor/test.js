192b16853dcba570150d98e9149f607d1f811a3e
git <lipusipupara@gmail.com>
2016-02-29 23:26:44 +0530

    some server code

--- protractor/test.js
+++ protractor/test.js
@@ -1,1 +1,14 @@
-var webdriver =require("webdriverio");
+var webdriver = require("webdriverio");
+var option = {
+    desiredCapabilities: {
+        browserName: "firefox"
+    }
+};
+
+webdriver
+    .remote(option)
+    .init()
+    .url("http://www.google.com")
+    .title(function (err, res) {
+        console.log("the value " + res.value);
+    }).end();
